0f2253c90bfac9c449b0ecfd3abc1d307ad64775
Juan Ignacio Mazza <juanignaciomazza@hotmail.com>
2023-08-29 20:02:40 -0300

    pre-entrega primera entrega final

--- src/app.js
+++ src/app.js
@@ -1,43 +1,17 @@
-const ProductManager = require ("./productManager.js");
-const manager = new ProductManager("./fileProducts/products.json")
+import express from "express"
+import productRouter from "./routes/products.router.js"
+import cartRouter from "./routes/carts.router.js"
 
-const express = require("express")
-const app = express()
+const app = express();
 const PORT = 8080;
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get("/products", async (req,res) => {
-
-    const { limit } = req.query
-    const products = await manager.getProducts()
-
-    if (limit) {
-
-        const limitproducts = products.slice(0,limit)
-        res.json({status:"Success", limitproducts})
-
-    }
-    else {
-
-        res.json({status:"Success", products})
-
-    }
-})
-
-app.get("/products/:pid", async (req,res) => {
-
-    const { pid } = req.params
-    const products = await manager.getProducts()
-    const productfind = products.find(el => el.id === parseInt(pid))
-    
-    console.log(productfind)
-    res.send({status: "success", productfind})
-
-})
+app.use("/api", productRouter)
+app.use("/api", cartRouter)
 
 
-app.listen(PORT,()=>{
-    console.log("server is working")
+app.listen(PORT, () => {
+    console.log("Server is working ")
 })
